acc61a74f8dd5ef614016f68685bb02fc908989d
manshina <dymokgames@gmail.com>
2022-10-04 19:52:17 +0300

    Добавлен Sql запрос

---
+++ SQLQuery.sql
@@ -0,0 +1,4 @@
+SELECT Products.Name, Categories.Name
+FROM Products
+LEFT JOIN ProductCategory ON Products.Id = ProductCategory.ProductId
+LEFT JOIN Categories ON ProductCategory.CategoryId = Categories.Id
